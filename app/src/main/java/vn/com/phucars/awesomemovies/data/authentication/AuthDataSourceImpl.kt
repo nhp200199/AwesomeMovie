3b280be27721a32f70613f366779c3d3f0d0b05c
Phuc Nguyen <nhp200199@gmail.com>
2023-11-26 17:36:10 +0700

    change dispatcher for AuthDataSource

--- app/src/main/java/vn/com/phucars/awesomemovies/data/authentication/AuthDataSourceImpl.kt
+++ app/src/main/java/vn/com/phucars/awesomemovies/data/authentication/AuthDataSourceImpl.kt
@@ -6,6 +6,9 @@ import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import vn.com.phucars.awesomemovies.data.ResultData
 import vn.com.phucars.awesomemovies.data.common.exception.AuthEmailMalformedException
 import vn.com.phucars.awesomemovies.data.common.exception.AuthInvalidUserEmailException
@@ -18,61 +21,71 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-class AuthDataSourceImpl(private val firebaseAuth: FirebaseAuth, private val userMapper: Mapper<FirebaseUser, AuthUser>) : AuthDataSource {
+class AuthDataSourceImpl(
+    private val firebaseAuth: FirebaseAuth,
+    private val userMapper: Mapper<FirebaseUser, AuthUser>,
+    private val dispatcher: CoroutineDispatcher
+) : AuthDataSource {
     override suspend fun register(email: String, password: String): ResultData<AuthUser> {
-        return suspendCoroutine<ResultData<AuthUser>> { cont ->
-            firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener() { task ->
-                    if (task.isSuccessful) {
-                        firebaseAuth.currentUser
-                        cont.resumeWith(Result.success(ResultData.Success(userMapper.map(firebaseAuth.currentUser!!))))
-                    } else {
-                        val exception: Exception = when(task.exception) {
-                            is FirebaseAuthWeakPasswordException -> {
-                                AuthWeakPasswordException((task.exception as FirebaseAuthWeakPasswordException).localizedMessage!!)
+        return withContext(dispatcher) {
+            suspendCoroutine<ResultData<AuthUser>> { cont ->
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener() { task ->
+                        if (task.isSuccessful) {
+                            firebaseAuth.currentUser
+                            cont.resumeWith(Result.success(ResultData.Success(userMapper.map(firebaseAuth.currentUser!!))))
+                        } else {
+                            val exception: Exception = when(task.exception) {
+                                is FirebaseAuthWeakPasswordException -> {
+                                    AuthWeakPasswordException((task.exception as FirebaseAuthWeakPasswordException).localizedMessage!!)
+                                }
+                                is FirebaseAuthInvalidCredentialsException -> {
+                                    AuthEmailMalformedException((task.exception as AuthEmailMalformedException).localizedMessage!!)
+                                }
+                                is FirebaseAuthUserCollisionException -> {
+                                    AuthUserCollisionException((task.exception as AuthUserCollisionException).localizedMessage!!)
+                                }
+                                else -> {
+                                    UnknownException()
+                                }
                             }
-                            is FirebaseAuthInvalidCredentialsException -> {
-                                AuthEmailMalformedException((task.exception as AuthEmailMalformedException).localizedMessage!!)
-                            }
-                            is FirebaseAuthUserCollisionException -> {
-                                AuthUserCollisionException((task.exception as AuthUserCollisionException).localizedMessage!!)
-                            }
-                            else -> {
-                                UnknownException()
-                            }
+                            cont.resumeWith(Result.success(ResultData.Error(exception)))
                         }
-                        cont.resumeWith(Result.success(ResultData.Error(exception)))
                     }
-                }
+            }
         }
     }
 
     override suspend fun login(email: String, password: String): ResultData<AuthUser> {
-        return suspendCoroutine<ResultData<AuthUser>> { cont ->
-            firebaseAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener() { task ->
-                    if (task.isSuccessful) {
-                        firebaseAuth.currentUser
-                        cont.resumeWith(Result.success(ResultData.Success(userMapper.map(firebaseAuth.currentUser!!))))
-                    } else {
-                        val exception: Exception = when(task.exception) {
-                            is FirebaseAuthInvalidUserException -> {
-                                AuthInvalidUserEmailException()
+        return withContext(dispatcher) {
+            suspendCoroutine<ResultData<AuthUser>> { cont ->
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener() { task ->
+                        if (task.isSuccessful) {
+                            firebaseAuth.currentUser
+                            cont.resumeWith(Result.success(ResultData.Success(userMapper.map(firebaseAuth.currentUser!!))))
+                        } else {
+                            val exception: Exception = when(task.exception) {
+                                is FirebaseAuthInvalidUserException -> {
+                                    AuthInvalidUserEmailException()
+                                }
+                                is FirebaseAuthInvalidCredentialsException -> {
+                                    AuthInvalidUserPasswordException()
+                                }
+                                else -> {
+                                    UnknownException()
+                                }
                             }
-                            is FirebaseAuthInvalidCredentialsException -> {
-                                AuthInvalidUserPasswordException()
-                            }
-                            else -> {
-                                UnknownException()
-                            }
+                            cont.resumeWith(Result.success(ResultData.Error(exception)))
                         }
-                        cont.resumeWith(Result.success(ResultData.Error(exception)))
                     }
-                }
+            }
         }
     }
 
     override suspend fun logout() {
-        firebaseAuth.signOut()
+        withContext(dispatcher) {
+            firebaseAuth.signOut()
+        }
     }
 }
